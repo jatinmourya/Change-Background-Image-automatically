5051ce48e2ace493ff3b1c71fc537e9ab8abd92f
Jatin mourya <jatinmourya07798@gmail.com>
2022-02-18 17:46:43 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+var img = [
+      "/HTML/coffee/images/c2.jpg",
+      "/HTML/coffee/images/c3.jpeg",
+      "/HTML/coffee/images/c4.jpeg",
+      "/HTML/coffee/images/c1.jpeg",
+      ];
+      var counter = 0;
+      changeImage = ()=> {
+        document.body.style.backgroundImage = "url(" + img[counter] + ")";
+        counter++;
+        if (counter == 4) {
+          counter = 0;
+        }
+      }
+      setInterval(changeImage, 2000);
